dd2918c7133c44c749d3c6d174aba45d34d15aa9
rndrps <filipp.sitdikov@gmail.com>
2022-10-25 19:54:23 +0300

    Update SmartTable.controller.js

--- webapp/controller/SmartTable.controller.js
+++ webapp/controller/SmartTable.controller.js
@@ -1,10 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (Controller){
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel){
     "use strict";
     return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
         onInit: function() {
-             this.getView().bindElement("/ztestStr001Set('0002')");
+            //  this.getView().bindElement("/ztestStr001Set('0002')");
+            var oModel = new sap.ui.model.json.JSONModel([{
+                "ID": 1000
+            }]);
+            this.getView().setModel(oModel);
           }
     });
 });
