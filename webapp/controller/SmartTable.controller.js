b9e11b40a58c5ae56769967f116229306e97ac93
rndrps <filipp.sitdikov@gmail.com>
2022-10-25 19:13:38 +0300

    meta

---
+++ webapp/controller/SmartTable.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+], function (Controller){
+    "use strict";
+    return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
+        onInit: function() {
+            // this.getView().bindElement("/Products('4711')");
+          }
+    });
+});
