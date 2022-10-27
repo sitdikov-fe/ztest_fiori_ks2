a623e3ac101092a87a4798d6dce45a4d8967804d
rndrps <filipp.sitdikov@gmail.com>
2022-10-27 15:48:36 +0300

    4332432

--- webapp/controller/SmartTable.controller.js
+++ webapp/controller/SmartTable.controller.js
@@ -5,7 +5,8 @@ sap.ui.define([
     "use strict";
     return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
         onInit: function() {
-            
+            oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/", true);
+			this.getView().byId("oSelectClient").setModel(oModel);
           }
     });
 });
