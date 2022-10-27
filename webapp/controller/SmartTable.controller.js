833f05a6b9faf56abf5ddd3c9ca951233d64f585
rndrps <filipp.sitdikov@gmail.com>
2022-10-27 17:33:49 +0300

    54363546

--- webapp/controller/SmartTable.controller.js
+++ webapp/controller/SmartTable.controller.js
@@ -5,9 +5,38 @@ sap.ui.define([
     "use strict";
     var oModel;
     return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
-        onInit: function() {
-            oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/", true);
-            this.getView().byId("oSelectData").setModel(oModel);
-          }
+        // onInit: function() {
+        //     oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/", true);
+        //     this.getView().byId("oSelectData").setModel(oModel);
+        //   }
+        onInit: function () {
+
+			var oModel = this.getView().getModel();
+			var sSet = "/" + "DataSourceSet";
+			oModel.read(sSet, {
+				success: function (oData) {
+					var oModelMNA = new JSONModel();
+					oModelMNA.setData(oData.results);
+					this.getView().setModel(oModelMNA, "oModelMNA");
+				}.bind(this),
+				error: function (oResponse) {
+					sap.m.MessageToast.show("oData fetching failed");
+				}
+			});
+
+		},
+        onInitialise: function (oEvent) {
+
+			var oTable = oEvent.getSource().getTable();
+			var aColumns = oTable.getColumns();
+
+			for (var i = 0; i < aColumns.length; i++) {
+				var sPath = "oModelMNA>" + aColumns[i].data("p13nData").columnKey;
+				aColumns[i].getTemplate().getDisplay().bindText(sPath);
+				aColumns[i].getTemplate().getEdit().bindValue(sPath);
+			}
+
+			oTable.bindRows("oModelMNA>/");
+		}
     });
 });
