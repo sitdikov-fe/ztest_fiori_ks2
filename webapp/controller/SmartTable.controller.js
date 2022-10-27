da66d9e16735598ea80dcdf3026eb7c539769ceb
rndrps <filipp.sitdikov@gmail.com>
2022-10-27 17:42:49 +0300

    234234

--- webapp/controller/SmartTable.controller.js
+++ webapp/controller/SmartTable.controller.js
@@ -1,23 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel){
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast){
     "use strict";
-    var oModel;
+    // var oModel;
     return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
         // onInit: function() {
         //     oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/", true);
         //     this.getView().byId("oSelectData").setModel(oModel);
         //   }
         onInit: function () {
-
+            
 			var oModel = this.getView().getModel();
 			var sSet = "/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/";
 			oModel.read(sSet, {
 				success: function (oData) {
 					var oModelMNA = new JSONModel();
 					oModelMNA.setData(oData.results);
-					this.getView().setModel(oModelMNA, "oModelMNA");
+					this.getView().byId("oSelectData").setModel(oModelMNA, "oModelMNA");
 				}.bind(this),
 				error: function (oResponse) {
 					sap.m.MessageToast.show("oData fetching failed");
