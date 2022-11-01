76661acfe0ec242f651ea3d8f3a8e076bc1682e3
rndrps <filipp.sitdikov@gmail.com>
2022-11-01 13:37:00 +0300

    Update SmartTable.controller.js

--- webapp/controller/SmartTable.controller.js
+++ webapp/controller/SmartTable.controller.js
@@ -5,7 +5,7 @@ sap.ui.define([
 ], function (Controller, JSONModel, MessageToast){
     "use strict";
     var oModel;
-    var userdata;
+
     return Controller.extend("ztest_fiori_ks.controller.SmartTable", {
         onInit: function() {
             oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/", true);
