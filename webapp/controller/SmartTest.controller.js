c9801d9a35a0d145fce39972844d85cd2112a5df
rndrps <filipp.sitdikov@gmail.com>
2022-11-07 16:57:24 +0300

    4343

---
+++ webapp/controller/SmartTest.controller.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast){
+    "use strict";
+    var oModel;
+
+    return Controller.extend("ztest_fiori_ks.controller.SmartTest", {
+        onInit: function() {
+            oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/", true);
+            this.getView().byId("oSelectData").setModel(oModel);
+            console.log("init");
+            console.log(oModel);
+          },
+          onAddRow:  function(oData) {
+            var readurl = "/ztestStr001Set";
+
+            oModel.read(readurl, {
+				success : function(oData, oResponse) {
+                    // console.log("addrow");
+                    this.getView().byId("oSelectData").getModel().refresh(true);
+				}.bind(this)
+			});
+          }
+    });
+});
